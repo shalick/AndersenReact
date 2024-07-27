bbb11d484e8818115d4a6ce63feb4319162ab65f
Alexander Shabanovich <alikshabanovich@gmail.com>
2024-07-27 14:26:19 +0300

    feat: add start card component

--- Quiz/src/App.tsx
+++ Quiz/src/App.tsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Start from "./components/Start/Start";
 
 function App() {
   return (
     <div className="wrapper">
       <Header />
-      <main className="container"></main>
+      <main className="container">
+        <Start />
+      </main>
       <Footer />
     </div>
   );
---
+++ Quiz/src/components/Start/Start.tsx
@@ -0,0 +1,43 @@
+import "./Start.css";
+import { InputNumber } from "antd";
+import { Select, Button } from "antd";
+
+const { Option } = Select;
+
+const Start = () => {
+  return (
+    <div className="card">
+      <h2 className="settings_title">Settings:</h2>
+      <div className="settings_select">
+        <InputNumber min={5} max={15} defaultValue={5} />
+        <Select
+          placeholder="category"
+          style={{ width: 120 }}
+          options={[{ value: "category", label: <span>category</span> }]}
+        />
+        <Select
+          placeholder="difficulty"
+          style={{ width: 120 }}
+          options={[{ value: "difficulty", label: <span>difficulty</span> }]}
+        />
+        <Select
+          placeholder="type"
+          style={{ width: 120 }}
+          options={[{ value: "type", label: <span>type</span> }]}
+        />
+        <Select placeholder="time" style={{ width: 120 }}>
+          <Option value="1m">1m</Option>
+          <Option value="2m">2m</Option>
+          <Option value="5m">5m</Option>
+        </Select>
+      </div>
+
+      <div className="start_buttons">
+        <Button>Start quiz</Button>
+        <Button>See my stats</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Start;
