78bb1356ca9cc72010ead9387526d30964aae6d9
Alexander Shabanovich <alikshabanovich@gmail.com>
2024-07-27 13:56:55 +0300

    feat: add basic layout

---
+++ Quiz/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <main className="container"></main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ Quiz/src/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <a href="https://github.com/shalick/AndersenReact/">
+        © 2024 Alick Shabanovich
+      </a>
+    </div>
+  );
+};
+// const Footer = () => (
+
+// );
+
+export default Footer;
